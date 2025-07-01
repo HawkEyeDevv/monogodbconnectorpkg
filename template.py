546e82083a7185454742dc2f79313548c51c5f2d
HawkEyeDev <draculemiihawk09@gmail.com>
2025-07-01 16:14:28 +0530

    first commit

---
+++ template.py
@@ -0,0 +1,31 @@
+import os.path
+from pathlib import Path
+
+list_of_files = [
+    ".github/workflows",
+    "src/__init__.py",
+    "src/mongodb-connect.py",
+    "experiment/__init__.py",
+    "experiment/experiments.ipynb",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/unit/unit.py",
+    "tests/integration/__init__.py",
+    "tests/integration/int.py",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "tox.ini",
+    "pyproject.toml",
+    "setup.cfg",
+    "setup.py",
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    dir_name, file_name = os.path.split(file_path)
+    if dir_name != "":
+        os.makedirs(dir_name, exist_ok=True)
+
+    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        with open(file_path, "w") as f:
+            pass
